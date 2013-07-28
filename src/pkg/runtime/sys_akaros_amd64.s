89fb590968caa4e17817d82217cdd375fba52b40
Kevin Klues <klueska@cs.berkeley.edu>
2013-07-28 16:07:38 -0700

    Remove contents of amd64 settls since e never call it

--- src/pkg/runtime/sys_akaros_amd64.s
+++ src/pkg/runtime/sys_akaros_amd64.s
@@ -6,11 +6,7 @@
 // System calls and other sys stuff for Akaros amd64
 //
 
-// set tls base to DI
-TEXT runtime·settls(SB),7,$32
-	SUBQ	$12, SP
-	MOVL	$0, 0(SP)  // vcore 0
-	MOVQ	DI, 4(SP)  // the new fs base
-	CALL	runtime∕parlib·Set_tls_desc(SB)
+// Do nothing for now
+TEXT runtime·settls(SB), 7, $0
 	RET
 
