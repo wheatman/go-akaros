76cb364cbf7b2755bc01fb2e1acf749b5dddd0fd
Kevin Klues <klueska@cs.berkeley.edu>
2013-07-29 21:36:52 -0700

    Enable timeouts for futexes

--- src/pkg/runtime/parlib/gcc_akaros.c
+++ src/pkg/runtime/parlib/gcc_akaros.c
@@ -13,10 +13,9 @@ const gcc_call_t gcc_syscall = (gcc_call_t)ros_syscall_sync;
 // Akaros style futexes
 static void __gcc_futex(void *__arg)
 {
-	// For now, akaros futexes don't support timeout, uaddr2 or val3, so we
+	// For now, akaros futexes don't support uaddr2 or val3, so we
 	// just 0 them out.
 	gcc_futex_arg_t *a = (gcc_futex_arg_t*)__arg;
-	a->timeout = NULL;
 	a->uaddr2 = NULL;
 	a->val3 = 0;
 	a->retval = futex(a->uaddr, a->op, a->val, a->timeout, a->uaddr2, a->val3);
