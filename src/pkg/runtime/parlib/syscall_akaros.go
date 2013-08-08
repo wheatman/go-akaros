d8f180649c61b2d8c5c7997d8dc1e32b009bdca2
Kevin Klues <klueska@cs.berkeley.edu>
2013-08-07 23:56:26 -0700

    Update go syscalls to use ros_syscall_sync
    They are also now aware of the errstr but they just throw it away since
    it's not yet used by Go.

--- src/pkg/runtime/parlib/syscall_akaros.go
+++ src/pkg/runtime/parlib/syscall_akaros.go
@@ -10,18 +10,21 @@ package parlib
 import "C"
 import "unsafe"
 
-func Syscall(_num uint32,
-             _a0 int, _a1 int,
-             _a2 int, _a3 int,
-             _a4 int, _a5 int,
-             errno_loc *int32, ret *int) {
-	__ret := int(C.__ros_syscall(C.uint(_num),
-	                             C.long(_a0), C.long(_a1),
-	                             C.long(_a2), C.long(_a3),
-	                             C.long(_a4), C.long(_a5),
-	                             (*C.int)(unsafe.Pointer(errno_loc))))
-	if ret != nil {
-		*ret = __ret
-	}
-}
+type SyscallType C.struct_syscall
+const (
+	MAX_ERRSTR_LEN = C.MAX_ERRSTR_LEN
+)
 
+func Syscall(_num uint32, _a0, _a1, _a2, _a3, _a4, _a5 int) (ret int, err int32, errstr string) {
+	var syscall SyscallType
+	syscall.num = C.uint(_num)
+	syscall.ev_q = (*C.struct_event_queue)(unsafe.Pointer(nil))
+	syscall.arg0 = C.long(_a0)
+	syscall.arg1 = C.long(_a1)
+	syscall.arg2 = C.long(_a2)
+	syscall.arg3 = C.long(_a3)
+	syscall.arg4 = C.long(_a4)
+	syscall.arg5 = C.long(_a5)
+	C.ros_syscall_sync((*C.struct_syscall)(unsafe.Pointer(&syscall)));
+	return int(syscall.retval), int32(syscall.err), C.GoString(&syscall.errstr[0]);
+}
