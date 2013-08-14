18e7e70ecc692ddb6ea481b1aac0835bb288d596
Kevin Klues <klueska@cs.berkeley.edu>
2013-08-14 14:26:10 -0700

    Update futex to allow timeouts to be passed

--- src/pkg/runtime/parlib/funcs.go
+++ src/pkg/runtime/parlib/funcs.go
@@ -18,11 +18,18 @@ import "unsafe"
 
 func Futex(uaddr *int32, op int32, val int32,
            timeout *Timespec, uaddr2 *int32, val3 int32) (ret int32) {
-	// For now, akaros futexes don't support timeout, uaddr2 or val3, so we
+	// For now, akaros futexes don't support uaddr2 or val3, so we
 	// just 0 them out.
-	timeout = nil
-	uaddr2 = nil
-	val3 = 0
+	uaddr2 = nil;
+	val3 = 0;
+	// Also, the minimum timout is 1ms, so up it to that if it's too small
+	if (timeout != nil) {
+		if (timeout.tv_sec == 0) {
+			if (timeout.tv_nsec < 1000000) {
+				timeout.tv_nsec = 1000000;
+			}
+		}
+	}
 	return int32(C.futex((*C.int)(unsafe.Pointer(uaddr)),
 	                     C.int(op), C.int(val),
 	                     (*C.struct_timespec)(unsafe.Pointer(timeout)),
