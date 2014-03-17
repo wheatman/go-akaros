76e645faac5913f1973eaf145086bb1de2944d4e
Kevin Klues <klueska@cs.berkeley.edu>
2014-03-17 11:39:29 -0700

    Changed the meaning of RawSyscall for generated syscalls on Akaros
    Akaros syscalls are all made through the parlib.Syscall() interface.  If
    a syscall follows the general form dictated for syscalls in
    mksyscall.pl, then we can automatically generate the syscalls using a
    //sys or //sysnb directive.  The difference between //sys and //sysnb is
    whether the generated syscall makes an underlying call to Syscall() or
    RawSyscall().  Traditionally, the RawSyscall version has been used to
    generate NON-BLOCKING versions of a syscall.  In Akaros, however, ALL
    syscalls are non-blocking, so we use //sysnb for a different purpose.
    
    Regular syscalls check the return value (i.e. __r1 == -1) to determine
    if they should construct an AkaError and return an error. RawSyscalls
    check the value of errno (i.e. __err == 0) to determine whether an error
    has occured.
    
    In practice, most syscalls return -1 to signify an error, and may or may
    not have errno set if the call is successful (in which case, checking
    errno instead of the return value may indicate an error even if there
    wasn't one).  However, some calls (like mmap) don't return -1 on error
    so we need a different way of determining if an error occured.  Syscalls
    of this type MUST only set errno if there was actually an error,
    otherwise we may be screwed.  Seems to be fine for now. We plan to
    standardize how all error handing is doen in Akaros soon, and when that
    happens, having to make this distinction will be unnecessary.

--- src/pkg/syscall/asm_akaros.s
+++ src/pkg/syscall/asm_akaros.s
@@ -1,19 +1,3 @@
-// Since we make all of our syscalls through the parlib interface fo for
-// Akaros, I simply jump to the syscall wrappers provided in syscall_akaros.go
-// which properly cast the arguments before sending them off to parlib.
-//
-// Additionally, Akaros sycalls NEVER block, so it's ok to implement regular
-// syscalls the same as raw ones...
-
-TEXT	·Syscall(SB),7,$0
-	JMP	syscall·SyscallWrapper(SB)
-
-TEXT	·Syscall6(SB),7,$0
-	JMP	syscall·Syscall6Wrapper(SB)
-
-TEXT	·RawSyscall(SB),7,$0
-	JMP	syscall·SyscallWrapper(SB)
-
-TEXT	·RawSyscall6(SB),7,$0
-	JMP	syscall·Syscall6Wrapper(SB)
+// For some reason, Go freaks out if this file doesn't exist.
+// We may need to add some things in here eventually, anyway.
 
