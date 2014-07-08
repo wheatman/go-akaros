f16c45435734a3d0fab1f3fa46e85f24a2a124ed
David Crawshaw <david.crawshaw@zentus.com>
2014-07-08 13:45:31 -0400

    cmd/addr2line: skip test on android
    LGTM=minux
    R=golang-codereviews, minux
    CC=golang-codereviews
    https://golang.org/cl/104600043

--- src/cmd/addr2line/addr2line_test.go
+++ src/cmd/addr2line/addr2line_test.go
@@ -92,8 +92,9 @@ func testAddr2Line(t *testing.T, exepath, addr string) {
 
 // This is line 93. The test depends on that.
 func TestAddr2Line(t *testing.T) {
-	if runtime.GOOS == "akaros" || runtime.GOOS == "nacl" {
-		t.Skip("skipping on " + runtime.GOOS)
+	switch runtime.GOOS {
+	case "nacl", "android", "akaros":
+		t.Skipf("skipping on %s", runtime.GOOS)
 	}
 
 	syms := loadSyms(t)
