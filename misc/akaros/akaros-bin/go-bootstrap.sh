b00d71127a7e581d0f08c7a9bec4e55166f04676
Kevin Klues <klueska@cs.berkeley.edu>
2014-07-15 13:26:02 -0700

    No need to copy the cpio file, and abstract port number out

--- misc/akaros/akaros-bin/go-bootstrap.sh
+++ misc/akaros/akaros-bin/go-bootstrap.sh
@@ -3,6 +3,7 @@
 AKAROS_MNT=/mnt
 AKAROS_GOROOT=/go
 CPIO_FILE=$AKAROS_MNT/go.cpio
+LISTEN_PORT=5555
 
 # Make sure we are at /
 cd /
@@ -13,16 +14,13 @@ ash root $@
 # Create $AKAROS_GOROOT
 mkdir -p ${AKAROS_GOROOT#/}
 
-# Copy over the archived go tree into /
-cp $CPIO_FILE /
-
-# Extract it into $AKAROS_GOROOT
+# Extract the $CPIO_FILE into $AKAROS_GOROOT
 cd $AKAROS_GOROOT
 cpio -d -i < $CPIO_FILE
-
-# Go back to / and start the listen server from there
 cd /
 
 # Start the listen daemon to wait for incoming rpc calls
-listen1 tcp!*!23 /bin/ash &
+listen1 tcp!*!$LISTEN_PORT /bin/ash &
 
+# Start up ash so we can debug stuff if necessary
+ash
