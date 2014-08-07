f763addcc40adf5d345482e1eb59c4e618cae895
Ian Lance Taylor <iant@golang.org>
2014-08-07 12:38:39 -0700

    cmd/go: pass --build-id=none when generating a cgo .o
    Some systems, like Ubuntu, pass --build-id when linking.  The
    effect is to put a note in the output file.  This is not
    useful when generating an object file with the -r option, as
    it eventually causes multiple build ID notes in the final
    executable, all but one of which are for tiny portions of the
    file and are therefore useless.
    
    Disable that by passing an explicit --build-id=none when
    linking with -r on systems that might do this.
    
    LGTM=bradfitz
    R=golang-codereviews, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/119460043

--- src/cmd/go/build.go
+++ src/cmd/go/build.go
@@ -2359,7 +2359,7 @@ func (b *builder) cgo(p *Package, cgoExe, obj string, gccfiles, gxxfiles, mfiles
 	// systems likely to support it, which is to say, systems that
 	// normally use gold or the GNU linker.
 	switch goos {
-	case "android", "dragonfly", "linux", "netbsd":
+	case "akaros", "android", "dragonfly", "freebsd", "linux", "netbsd", "openbsd":
 		ldflags = append(ldflags, "-Wl,--build-id=none")
 	}
 
