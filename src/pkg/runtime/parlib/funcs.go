24e42ebb2867784d7dc5a5c4dfe7ef7b0ca67db8
Kevin Klues <klueska@cs.berkeley.edu>
2014-08-13 16:02:35 -0700

    Remove cgo calls to chmod and fchmod
    These are now implemented directly with wstat

--- src/pkg/runtime/parlib/funcs.go
+++ src/pkg/runtime/parlib/funcs.go
@@ -80,20 +80,3 @@ func Errno() (int) {
 func Errstr() (string) {
 	return C.GoString(C.go_parlib_errstr())
 }
-
-func Chmod(path string, mode uint32) (int) {
-	return int(C.chmod(C.CString(path), C.__mode_t(mode)))
-}
-
-func Fchmod(fd int, mode uint32) (int) {
-	return int(C.fchmod(C.int(fd), C.__mode_t(mode)))
-}
-
-func Truncate(path string, size int64) (int) {
-	return int(C.truncate64(C.CString(path), C.__off64_t(size)))
-}
-
-func Ftruncate(fd int, size int64) (int) {
-	return int(C.ftruncate64(C.int(fd), C.__off64_t(size)))
-}
-
