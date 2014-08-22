09f358a28bdba358b2a324d338a3d0795a155a7a
Kevin Klues <klueska@cs.berkeley.edu>
2014-08-22 12:07:55 -0700

    Support both Hw and Sw trapframes for contexts
    The context passed to a singal may be stored in either a hardware or a
    software trapframe, so wee need to make sure we extract the info from it
    at the right offsets.  Since KenC doesn't support nice macro
    preprocessing, we are left with this monstrosity.  Shouldn't ever change
    though (or at least not often), so it's fine.

--- src/pkg/runtime/signal_akaros_amd64.h
+++ src/pkg/runtime/signal_akaros_amd64.h
@@ -2,31 +2,88 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#define SIG_REGS(ctxt) (*((HwTrapframe*)(&((((struct UserContext*)ctxt)->tf)[0]))))
-
-#define SIG_RAX(info, ctxt) (SIG_REGS(ctxt).tf_rax)
-#define SIG_RBX(info, ctxt) (SIG_REGS(ctxt).tf_rbx)
-#define SIG_RCX(info, ctxt) (SIG_REGS(ctxt).tf_rcx)
-#define SIG_RDX(info, ctxt) (SIG_REGS(ctxt).tf_rdx)
-#define SIG_RDI(info, ctxt) (SIG_REGS(ctxt).tf_rdi)
-#define SIG_RSI(info, ctxt) (SIG_REGS(ctxt).tf_rsi)
-#define SIG_RBP(info, ctxt) (SIG_REGS(ctxt).tf_rbp)
-#define SIG_RSP(info, ctxt) (SIG_REGS(ctxt).tf_rsp)
-#define SIG_R8(info, ctxt) (SIG_REGS(ctxt).tf_r8)
-#define SIG_R9(info, ctxt) (SIG_REGS(ctxt).tf_r9)
-#define SIG_R10(info, ctxt) (SIG_REGS(ctxt).tf_r10)
-#define SIG_R11(info, ctxt) (SIG_REGS(ctxt).tf_r11)
-#define SIG_R12(info, ctxt) (SIG_REGS(ctxt).tf_r12)
-#define SIG_R13(info, ctxt) (SIG_REGS(ctxt).tf_r13)
-#define SIG_R14(info, ctxt) (SIG_REGS(ctxt).tf_r14)
-#define SIG_R15(info, ctxt) (SIG_REGS(ctxt).tf_r15)
-#define SIG_RIP(info, ctxt) (SIG_REGS(ctxt).tf_rip)
-#define SIG_RFLAGS(info, ctxt) ((uint64)SIG_REGS(ctxt).tf_rflags)
-
-#define SIG_CS(info, ctxt) ((uint64)SIG_REGS(ctxt).tf_cs)
-#define SIG_FS(info, ctxt) ((uint64)SIG_REGS(ctxt).tf_fsbase)
-#define SIG_GS(info, ctxt) ((uint64)SIG_REGS(ctxt).tf_gsbase)
+#define ROS_HW_CTX              1                                                        |#endif
+#define ROS_SW_CTX              2
 
 #define SIG_CODE0(info, ctxt) ((info)->si_code)
 #define SIG_CODE1(info, ctxt) (*((uintptr*)(&((info)->si_addr))))
+
+// Registers in both the HW and SW trapframes
+// Safe to use as an l-value
+#define SIG_GS(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_gsbase) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_gsbase))))
+#define SIG_FS(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_fsbase) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_fsbase))))
+#define SIG_RBX(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rbx) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rbx))))
+#define SIG_RBP(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rbp) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rbp))))
+#define SIG_R12(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r12) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r12))))
+#define SIG_R13(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r13) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r13))))
+#define SIG_R14(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r14) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r14))))
+#define SIG_R15(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r15) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r15))))
+#define SIG_RIP(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rip) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rip))))
+#define SIG_RSP(info, ctxt) \
+	(*((uint64*)((((struct UserContext*)(ctxt))->type == 1) ? \
+		&(((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rsp) : \
+		&(((SwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rsp))))
+
+// Registers only in the HW trapframe (0'd out if dealing with sw trapframe)
+// Can't be used as an l-value
+#define SIG_RAX(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rax : 0)
+#define SIG_RCX(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rcx : 0)
+#define SIG_RDX(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rdx : 0)
+#define SIG_RDI(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rdi : 0)
+#define SIG_RSI(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rsi : 0)
+#define SIG_R8(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r8 : 0)
+#define SIG_R9(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r9 : 0)
+#define SIG_R10(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r10 : 0)
+#define SIG_R11(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_r11 : 0)
+#define SIG_CS(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_cs : 0)
+#define SIG_RFLAGS(info, ctxt) \
+	((((struct UserContext*)(ctxt))->type == 1) ? \
+		((HwTrapframe*)(&((((UserContext*)(ctxt))->tf)[0])))->tf_rflags : 0)
 
