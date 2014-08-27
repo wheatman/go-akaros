83bf4d049d4d55f5cb526bd5ebec6c7a8165b0fa
Kevin Klues <klueska@cs.berkeley.edu>
2014-08-27 10:42:02 -0700

    Remove unused directive
    We used to use a local.bash to set up our environment for building Go
    for akaros.  Now everything is done thorugh a different setup, so this
    if statement in this file is unneeded anymore.  It was basically a noop
    now anyway.

--- src/pkg/syscall/mkall_akaros.sh
+++ src/pkg/syscall/mkall_akaros.sh
@@ -1,9 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GOROOT="$DIR/../../.."
 SRCDIR="$GOROOT/src"
-if [ -f $SRCDIR/local.bash ]; then
-    source $SRCDIR/local.bash
-fi
 export GOROOT
 $DIR/mkall.sh $@
 rm -rf _obj
