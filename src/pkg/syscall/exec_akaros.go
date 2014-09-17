d6c7bb660f8e34680e3d3c7c1d6aafe4ed0691bd
Kevin Klues <klueska@cs.berkeley.edu>
2014-09-17 12:29:53 -0700

    Major cleanup of akaros fork/exec code
    We now longer have any fork/exec calls at all.  We support StartProcess
    directly.  We do support a direct Exec calls, but the implementation for
    Exec was wrong and I fixed it as part of this commit.
    
    One nice observation is taht we don't need the ForkLock in Akaros
    because we aren't forking and don't have races with file descriptors
    magically appearing / disappearing out from under us while executing a
    fork/exec command.

--- src/pkg/syscall/exec_akaros.go
+++ src/pkg/syscall/exec_akaros.go
@@ -1,8 +1,8 @@
-// Copyright 2011 The Go Authors. All rights reserved.
+// Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build akaros
+// StartProcess and Exec
 
 package syscall
 
@@ -11,39 +11,76 @@ import (
 	"runtime/parlib"
 )
 
-type SysProcAttr struct {
-	Chroot     string      // Chroot.
-	Credential *Credential // Credential.
-	Ptrace     bool        // Enable tracing.
-	Setsid     bool        // Create session.
-	Setpgid    bool        // Set process group ID to new pid (SYSV setpgrp)
-	Setctty    bool        // Set controlling terminal to fd Ctty (only meaningful if Setsid is set)
-	Noctty     bool        // Detach fd 0 from controlling terminal
-	Ctty       int         // Controlling TTY fd (Linux only)
-	Pdeathsig  Signal      // Signal that the process will get when its parent dies (Linux only)
+// ProcAttr holds attributes that will be applied to a new process started
+// by StartProcess.
+type ProcAttr struct {
+	Dir   string    // Current working directory.
+	Env   []string  // Environment.
+	Files []uintptr // File descriptors.
+	Sys   *SysProcAttr // System specific attrs
+}
+var zeroProcAttr ProcAttr
+
+// Undefined on Akaros
+type SysProcAttr struct {}
+var zeroSysProcAttr SysProcAttr
+
+// SlicePtrFromStrings converts a slice of strings to a slice of
+// pointers to NUL-terminated byte slices. If any string contains
+// a NUL byte, it returns (nil, EINVAL).
+func SlicePtrFromStrings(ss []string) ([]*byte, error) {
+	var err error
+	bb := make([]*byte, len(ss)+1)
+	for i := 0; i < len(ss); i++ {
+		bb[i], err = BytePtrFromString(ss[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+	bb[len(ss)] = nil
+	return bb, nil
 }
 
-// Fork, dup fd onto 0..len(fd), and exec(argv0, argvv, envv) in child.
-// If a dup or exec fails, write the errno error to pipe.
-// (Pipe is close-on-exec so if exec succeeds, it will be closed.)
-// In the child, this function must not acquire any locks, because
-// they might have been locked at the time of the fork.  This means
-// no rescheduling, no malloc calls, and no new stack segments.
-// The calls to RawSyscall are okay because they are assembly
-// functions that do not grow the stack.
-func forkAndExecInChild(argv0 []byte, argv, envv []*byte, chroot, dir []byte, attr *ProcAttr, sys *SysProcAttr) (pid int, err error) {
-	// Declare all variables at top in case any
-	// declarations require heap allocation (e.g., err1).
-	var (
-		r1     uintptr
-		err1   error
-	)
-
-	// Make sure we aren't passing invalid arguments for Akaros (we should
-	// probably support these some day though...)
-	if len(chroot) > 0 {
-		return 0, NewAkaError(EMORON, "Akaros does not support passing 'chroot' to forkAndExecInChild")
+func StartProcess(argv0 string, argv []string, attr *ProcAttr) (pid int, handle uintptr, err error) {
+	if attr == nil {
+		attr = &zeroProcAttr
 	}
+	sys := attr.Sys
+	if sys == nil {
+		sys = &zeroSysProcAttr
+	}
+
+	// Convert args to C form.
+	argv0p, err := ByteSliceFromString(argv0)
+	if err != nil {
+		return 0, 0, err
+	}
+	argvp, err := SlicePtrFromStrings(argv)
+	if err != nil {
+		return 0, 0, err
+	}
+	envvp, err := SlicePtrFromStrings(attr.Env)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	var dir []byte
+	if attr.Dir != "" {
+		dir, err = ByteSliceFromString(attr.Dir)
+		if err != nil {
+			return 0, 0, err
+		}
+	}
+
+	// Kick off child.
+	pid, err = startProcess(argv0p, argvp, envvp, dir, attr.Files)
+
+	// Return the pid and the error if there was one
+	return pid, 0, err
+}
+
+func startProcess(argv0 []byte, argv, envv []*byte, dir []byte, files []uintptr) (pid int, err error) {
+	var r1 uintptr
 
 	// Adjust argv0 to prepend 'dir' if argv0 is a relative path
 	if argv0[0] != '/' {
@@ -57,48 +94,69 @@ func forkAndExecInChild(argv0 []byte, argv, envv []*byte, chroot, dir []byte, at
 	cmdlen := uintptr(len(argv0))
 	__pi, _ := parlib.ProcinfoPackArgs(argv, envv)
 	pi := uintptr(unsafe.Pointer(&__pi))
-	r1, _, err1 = RawSyscall6(SYS_PROC_CREATE, cmd, cmdlen, pi, 0, 0, 0)
-	if err1 != nil {
-		return 0, err1
+	r1, _, err = RawSyscall6(SYS_PROC_CREATE, cmd, cmdlen, pi, 0, 0, 0)
+	if err != nil {
+		return 0, err
 	}
 	child := r1
 
 	// Dup the fd map properly into the child
-	__cfdm := make([]Childfdmap_t, len(attr.Files))
-	for i, f := range(attr.Files) {
+	__cfdm := make([]Childfdmap_t, len(files))
+	for i, f := range(files) {
 		__cfdm[i].Parentfd = uint32(f)
 		__cfdm[i].Childfd = uint32(i)
 		__cfdm[i].Ok = int32(-1)
 	}
 	cfdm := uintptr(unsafe.Pointer(&__cfdm[0]))
 	cfdmlen := uintptr(len(__cfdm))
-	r1, _, err1 = RawSyscall(SYS_DUP_FDS_TO, child, cfdm, cfdmlen)
-	if err1 != nil {
-		return 0, err1
+	r1, _, err = RawSyscall(SYS_DUP_FDS_TO, child, cfdm, cfdmlen)
+	if err != nil {
+		return 0, err
 	}
 
 	// If 'dir' passed in, set the pwd of the child
 	if len(dir) > 0 {
 		pwd := uintptr(unsafe.Pointer(&dir[0]))
 		pwdlen := uintptr(len(dir))
-		r1, _, err1 = RawSyscall(SYS_CHDIR, child, pwd, pwdlen)
-		if err1 != nil {
-			return 0, err1
+		r1, _, err = RawSyscall(SYS_CHDIR, child, pwd, pwdlen)
+		if err != nil {
+			return 0, err
 		}
 	}
 
 	// Now run the child!
-	r1, _, err1 = RawSyscall(SYS_PROC_RUN, child, 0, 0)
-	if err1 != nil {
-		return 0, err1
+	r1, _, err = RawSyscall(SYS_PROC_RUN, child, 0, 0)
+	if err != nil {
+		return 0, err
 	}
 
 	// Return the child pid
 	return int(child), nil
 }
 
-// Try to open a pipe with O_CLOEXEC set on both file descriptors.
-func forkExecPipe(p []int) (err error) {
-	err = Pipe(p, O_CLOEXEC)
-	return
+// Ordinary exec.
+func Exec(argv0 string, argv []string, envv []string) (err error) {
+	// Convert args to C form.
+	argv0p, err := ByteSliceFromString(argv0)
+	if err != nil {
+		return err
+	}
+	argvp, err := SlicePtrFromStrings(argv)
+	if err != nil {
+		return err
+	}
+	envvp, err := SlicePtrFromStrings(envv)
+	if err != nil {
+		return err
+	}
+
+	// exec to new cmd
+	cmd := uintptr(unsafe.Pointer(&argv0p[0]))
+	cmdlen := uintptr(len(argv0))
+	__pi, _ := parlib.ProcinfoPackArgs(argvp, envvp)
+	pi := uintptr(unsafe.Pointer(&__pi))
+	_, _, err = RawSyscall(SYS_EXEC, cmd, cmdlen, pi)
+	return err
 }
+
+func CloseOnExec(fd int) { fcntl(fd, F_SETFD, FD_CLOEXEC) }
