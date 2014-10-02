4be4faf0191e4c5695619e08696742c3db43db1c
Kevin Klues <klueska@gmail.com>
2014-10-02 00:29:25 -0700

    Make akaros specific implementation of memclr
    The default one uses XMM registers, which we don't seem to support
    properly yet.  The plan9 just segfaults for some reason, so we can't
    just pull that one in either.  I just copied the akaros kernel's version
    of memset in for now until we find a better solution.  At any rate, it's
    better than just looping over every byte.

---
+++ src/pkg/runtime/memclr_akaros.c
@@ -0,0 +1,74 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "runtime.h"
+#include "../../cmd/ld/textflag.h"
+
+// memset aligned words.
+static inline void *
+memsetw(intgo* _v, intgo c, uintptr n)
+{
+    intgo *start, *end, *v;
+
+    start = _v;
+    end = _v + n/sizeof(intgo);
+    v = _v;
+    c = c & 0xff;
+    c = c | c<<8;
+    c = c | c<<16;
+    #ifdef _64BIT
+    c = c | c<<32;
+    #endif
+
+    while(v < end - (8-1))
+    {
+        v[3] = v[2] = v[1] = v[0] = c;
+        v += 4;
+        v[3] = v[2] = v[1] = v[0] = c;
+        v += 4;
+    }
+
+    while(v < end)
+      *v++ = c;
+
+    return start;
+}
+
+static inline void *
+memset(byte* v, uint8 c, uintptr n)
+{
+    byte *p;
+    uintgo n0;
+
+	if (n == 0) return nil;
+
+	p = v;
+
+    while (n > 0 && ((uintptr)p & (sizeof(intgo)-1))) {
+        *p++ = c;
+        n--;
+    }
+
+    if (n >= sizeof(intgo)) {
+        n0 = n / sizeof(intgo) * sizeof(intgo);
+        memsetw((intgo*)p, c, n0);
+        n -= n0;
+        p += n0;
+    }
+
+    while (n > 0) {
+        *p++ = c;
+        n--;
+    }
+
+	return v;
+}
+
+// Runtime functions themselves
+#pragma textflag NOSPLIT
+void runtime·memclr(byte* p, uintptr n)
+{
+	memset(p, 0, n);
+}
+
